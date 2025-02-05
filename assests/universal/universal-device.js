3c8f180d288497be773e64588ae4f64804ef37fd
itsmarian <100617987+itsmarianmc@users.noreply.github.com>
2025-02-05 17:19:58 +0100

    Update universal-device.js

--- assests/universal/universal-device.js
+++ assests/universal/universal-device.js
@@ -1,12 +1,9 @@
 function detectDevice() {
     const userAgent = navigator.userAgent.toLowerCase();
     const isMobile = /iphone|ipod|android.*mobile|windows phone/.test(userAgent);
-    const isTablet = /ipad|android(?!.*mobile)/.test(userAgent);
     
     if (isMobile) {
         return "mobile";
-    } else if (isTablet) {
-        return "tablet";
     } else {
         return "desktop";
     }
@@ -16,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const deviceType = detectDevice();
     
     document.getElementById("mobile").style.display = deviceType === "mobile" ? "block" : "none";
-    document.getElementById("desktop").style.display = deviceType === "tablet" ? "block" : "none";
     document.getElementById("desktop").style.display = deviceType === "desktop" ? "block" : "none";
 });
 
