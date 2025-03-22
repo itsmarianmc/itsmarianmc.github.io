90cdce43f1951f7347bbd7b50f44c2a5309532f5
itsmarian <marian.duettra@gmail.com>
2025-03-22 17:33:51 +0100

    - assests/header.css - assests/header.js - assests/logos/Skin.png - assests/pages/mainpage/style.css - assests/style.css - assests/universal/universal-device.js - index.html

--- assests/universal/universal-device.js
+++ assests/universal/universal-device.js
@@ -1,19 +1,42 @@
-function detectDevice() {
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        console.log('%cWARNING!', 'color: red; font-size: 30px; font-weight: bold;');
+        console.log('%cThis is a browser feature intended for developers and debuggers only and may contain sensitive links and information about you, your data, account/s, device and browser. \nScammers have been known to encourage people to copy and/or paste information or run commands on the command line to hack accounts or access sensitive data. If you do not know what you are doing, do not proceed and close the debug menu! \nThe information that is/will be visible above and below this text is only for the development and improvement of the site and helps to find and fix bugs and other problems in JavaScript faster. \nAs this is a website related to, created, designed and managed by Projekt City please visit the folowing adress for more information: https://projektcity.github.io/helpcenter/debug-menu', 'color: red; font-size: 12.5px;');
+    }, 250);
+})
+
+function getDeviceType() {
     const userAgent = navigator.userAgent.toLowerCase();
-    const isMobile = /iphone|ipod|android.*mobile|windows phone/.test(userAgent);
-    
-    if (isMobile) {
-        return "mobile";
+    if (/mobile|android|iphone|ipad|ipod|blackberry|windows phone/i.test(userAgent)) {
+       return "mobile";
     } else {
         return "desktop";
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const deviceType = detectDevice();
-    
-    document.getElementById("mobile").style.display = deviceType === "mobile" ? "block" : "none";
-    document.getElementById("desktop").style.display = deviceType === "desktop" ? "block" : "none";
-});
+function getBrowser() {
+    const userAgent = navigator.userAgent;
+    if (userAgent.indexOf("Firefox") > -1) {
+        return "mf_mozilla-firefox";
+    } else if (userAgent.indexOf("SamsungBrowser") > -1) {
+        return "si_samsung-internet";
+    } else if (userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1) {
+        return "op_opera";
+    } else if (userAgent.indexOf("Trident") > -1) {
+        return "ie_internet-Explorer";
+    } else if (userAgent.indexOf("Edge") > -1) {
+        return "me_microsoft-edge";
+    } else if (userAgent.indexOf("Chrome") > -1) {
+        return "gc_google-chrome";
+    } else if (userAgent.indexOf("Safari") > -1) {
+        return "as_apple-safari";
+    } else {
+        return "uk_unknown";
+    }
+}
 
-console.log("set-userdevice=" + detectDevice());
+const deviceType = getDeviceType();
+const browser = getBrowser();
+
+console.log(`get-userdevice=${deviceType}`);
+console.log(`get-userbrowser=${browser}`);
