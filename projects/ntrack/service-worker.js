443fc250d6455b8bc9161ba447d1885d449e9e88
itsmarian <marian.duettra@gmail.com>
2025-05-13 21:01:40 +0200

    Update
    Affected file:
    - projects/ntrack/index.html
    - projects/ntrack/manifest.json
    - projects/ntrack/service-worker.js

--- projects/ntrack/service-worker.js
+++ projects/ntrack/service-worker.js
@@ -1,50 +1,13 @@
-const CACHE_NAME = 'hydrotracker-v1';
-const ASSETS_TO_CACHE = [
-    'favicon.png',
-    'index.html',
-    'manifest.json',
-    'style.css'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => 
-            Promise.all(keys.map(key => {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            }))
-        )
-    );
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => 
-            response || fetch(event.request)
-        )
-    );
-});self.addEventListener("install", event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("ntrack-cache").then(cache => {
       return cache.addAll([
-        "./",
-        "./favicon-dark.png",
-        "./favicon-light.png",
-        "./index.html",
-        "./styles.css",
-        "./script.js",
-        "./manifest.json"
+        "favicon-dark.png",
+        "favicon-light.png",
+        "index.html",
+        "styles.css",
+        "script.js",
+        "manifest.json"
       ]);
     }).catch(err => {
       console.error("Cache error during install:", err);
